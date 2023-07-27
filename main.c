fad109a4271c55b4f80030327bb2af8ea27a2475
PrinceMag <princeth.magutshwa@gmail.com>
2023-07-27 16:52:38 +0200

    restoring

--- main.c
+++ main.c
@@ -1,38 +1,34 @@
 #include "main.h"
 
 /**
-* main - function that execute the commands.
-* @argc: argument count
-* @argv: array containing the command and the options
-* Return: 0 on success.
-*/
-
-int main(int __attribute((unused)) argc, char **argv)
+ * main - Entry point of the shell program.
+ * @ac: Argument count.
+ * @av: Array of strings containing the arguments.
+ * @env: Array of strings containing the environment variables.
+ * Return: The exit status value.
+ **/
+int main(int ac, char *av[], char **env)
 {
-	char *input, **tokens, *cmd;
+    int count_exe = 1;
+    int status_exit = 0;
 
-	while (1)
-	{
-		if (isatty(STDIN_FILENO) == 1)
-			write(STDOUT_FILENO, "-> ", 3);
-		input = get_input();
-		if (!input)
-			continue;
-		tokens = tokenize(input);
-		if (builtin(tokens, input) == 0)
-		{
-			_free(2, input, tokens);
-			continue;
-		}
-		cmd = path_handler(tokens[0]);
-		if (cmd == NULL)
-		{
-			_write_err(argv[0], "command not found: ", tokens[0]);
-			_free(2, input, tokens);
-			continue;
-		}
-		execute(tokens, cmd);
-		_free(3, input, tokens, cmd);
-	}
-	return (0);
+    // Ignore unused argument warning for ac
+    (void)ac;
+
+    // Register signal handler for SIGINT (Ctrl+C)
+    signal(SIGINT, signal_handler);
+
+    // If there are additional arguments, run non-interactive mode
+    if (ac > 1)
+    {
+        status_exit = non_interact(av);
+        return status_exit;
+    }
+
+    // Otherwise, run interactive mode
+    status_exit = interactive(av, count_exe, env);
+    return status_exit;
 }
+
+
+
--- main.h
+++ main.h
@@ -1,48 +1,44 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdbool.h>
+/* Standard C Library Headers */
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <sys/wait.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+#include <signal.h>
 
-extern char **environ;
+/* Shell functions defined in shell_functions.c */
+int non_interact(char **av);
+int interactive(char *av[], int count_exe, char **env);
+int create_process(char *av[], int count_exe, char **env);
+char *getEnvironmentVariable(char *name, char **env);
+int print_env(char **env);
+void signal_handler(int sigint);
+int exitFunction(char *line, int status, char *av, int count);
+int _atoi(char *arr);
 
-#define MAX_BUFFER 2048
+/* Path functions defined in path_functions.c */
+char *validate_file(char *full_path, char *file);
 
-/**
- * struct Builtin - struct that holds count of
- * built in command in shell;
- * @cmd: name of the command
- * @func: pointer to function
- */
-typedef struct Builtin
-{
-	char *cmd;
-	void (*func)(void);
-} builtin_t;
+/* String functions defined in str_functions.c */
+char *_strcpy(char *dest, char *src);
+int _strlen(const char *s);
+char *_strcat(char *dest, char *src);
+char *duplicateString(char *str);
 
-/* Main functions*/
-char *get_input();
-char **tokenize(char *);
-int execute(char **argv, char *full_cmd);
-char *path_handler(char *arg);
-int builtin(char **args, char *input);
+/* String comparison functions defined in strcmp_functions.c */
+int _strcmp(char *s1, char *s2);
+int _strncmp(const char *s1, const char *s2, size_t n);
 
-/* Utility functions*/
-int _strlen(char *str);
-int _strlen_input(char *str);
-int get_tokens_count(char *input, char *delimiter);
-char *get_path();
-char *_strcat(char *, char *);
-void _write_err(char *shell, char *err, char *cmd);
-void _free(int count, ...);
-
-/* Builtin functions*/
-void _exit_(void);
-void _env_(void);
+/* Memory allocation functions */
+char *_memset(char *s, char b, unsigned int n);
+void *_calloc(unsigned int nmemb, int size);
+unsigned int length_array(char **p);
 
 #endif /* MAIN_H */
